edc19ecf52ed78fd5bd6b19ede531ae7ef6b4edd
mikuy517 <54749732+mikuy517@users.noreply.github.com>
2025-04-17 21:14:36 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
     // Proxy URL to bypass CORS policy
     const proxyURL = 'https://api.allorigins.win/get?url=';
-    const playlistURL = 'https://github.com/pratikkarbhal/Web-IPTV/blob/main/Sample.m3u?raw=true';
+    const playlistURL = 'https://pastebin.com/raw/RfutgD5p';
     const proxyPlaylistURL = proxyURL + encodeURIComponent(playlistURL);
 
     fetch(proxyPlaylistURL)
